773e4b73b22724a97a36fee2c2c859d58839cd34
Sam Cofer <samcofer@gmail.com>
2024-03-15 15:02:14 -0500

    Try Python

--- scripts/load-test.sh
+++ scripts/load-test.sh
@@ -18,19 +18,20 @@ mkdir -p /opt/scripts/
 cp ./* /opt/scripts/
 chmod -R 755 /opt/scripts/
 chown -R testuser:testuser /opt/results
+chown -R testuser:testuser /opt/scripts
 chmod -R 755 /opt/results/
 
 #Environment preparation
 
-# Python Installation
-#yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-#dnf install dnf-plugins-core
-#dnf config-manager --set-enabled "codeready-builder-for-rhel-8-*-rpms"
-#curl -O https://cdn.rstudio.com/python/centos-8/pkgs/python-${PYTHON_VERSION}-1-1.x86_64.rpm
-#sudo yum install python-${PYTHON_VERSION}-1-1.x86_64.rpm
-#/opt/python/"${PYTHON_VERSION}"/bin/pip install --upgrade pip setuptools wheel
-#export PATH=/opt/python/"${PYTHON_VERSION}"/bin:$PATH
-#
+ Python Installation
+yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+dnf install dnf-plugins-core
+dnf config-manager --set-enabled "codeready-builder-for-rhel-8-*-rpms"
+curl -O https://cdn.rstudio.com/python/centos-8/pkgs/python-${PYTHON_VERSION}-1-1.x86_64.rpm
+sudo yum install python-${PYTHON_VERSION}-1-1.x86_64.rpm
+/opt/python/"${PYTHON_VERSION}"/bin/pip install --upgrade pip setuptools wheel
+export PATH=/opt/python/"${PYTHON_VERSION}"/bin:$PATH
+
 ## R Installation
 #
 #curl -O https://cdn.rstudio.com/r/centos-8/pkgs/R-${R_VERSION}-1-1.x86_64.rpm
