273d75b09fb1e1b72c04df9e2b58cb5bbfe922d4
Sam Cofer <samcofer@gmail.com>
2024-03-19 12:39:48 -0500

    Add ioping tests

--- scripts/io-testing.sh
+++ scripts/io-testing.sh
@@ -8,12 +8,12 @@ nfsiostat | tee -a /opt/results/io-testing/nfsiostat-${DIRECTORY}
 for DIRECTORY in $TEST_DIRECTORIES
 do
 
-ioping -c 50 ${DIRECTORY} | tee -a /opt/results/io-testing/ioping-${DIRECTORY}
+ioping -c 50 /${DIRECTORY} | tee -a /opt/results/io-testing/ioping-${DIRECTORY}
 
-ioping -c 50 -s 1024k ${DIRECTORY} | tee -a /opt/results/io-testing/ioping-${DIRECTORY}
+ioping -c 50 -s 1024k /${DIRECTORY} | tee -a /opt/results/io-testing/ioping-${DIRECTORY}
 
-ioping -RAL -w 30 ${DIRECTORY} | tee -a /opt/results/io-testing/ioping-${DIRECTORY}
+ioping -RAL -w 30 /${DIRECTORY} | tee -a /opt/results/io-testing/ioping-${DIRECTORY}
 
-ioping -RWAL -w 30 ${DIRECTORY} | tee -a /opt/results/io-testing/ioping-${DIRECTORY}
+ioping -RWAL -w 30 /${DIRECTORY} | tee -a /opt/results/io-testing/ioping-${DIRECTORY}
 
 done
