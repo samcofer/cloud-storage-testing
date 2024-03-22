f59cee16c856a2fd7b7e46f128222916b10e8f0a
Sam Cofer <samcofer@gmail.com>
2024-03-22 12:12:48 -0500

    Fix app testing

--- scripts/app-testing.sh
+++ scripts/app-testing.sh
@@ -9,7 +9,7 @@ do
 # Test application locks
 
 touch /${DIRECTORY}/testfile
-../bin/locktester /${DIRECTORY}/testfile &
+/opt/cloud-storage-testing/bin/locktester /${DIRECTORY}/testfile &
 spawned_pid=$!
 timeout 10s ../bin/locktester /${DIRECTORY}/testfile | tee -a /opt/results/app-testing/${DIRECTORY}-lock-testing
 kill -9 $spawned_pid
@@ -17,9 +17,9 @@ kill -9 $spawned_pid
 # Test extended ACLs
 
 if setfacl -m u:ec2-user:x /${DIRECTORY}/testfile; then
-  echo "Extended ACLs are supported" > /opt/results/app-testing/${DIRECTORY}-extended-acl-supported
+  echo "Extended ACLs are supported" > /opt/results/app-testing/${DIRECTORY}-extended-acl-support
 else
-  echo "Extended ACLs are not supported" > /opt/results/app-testing/${DIRECTORY}-no-extended-acl-support
+  echo "Extended ACLs are not supported" > /opt/results/app-testing/${DIRECTORY}-extended-acl-support
 fi
 
 done
