b408dffbb7882d45be05cb0c2e809d9a3bc92ab6
Sam Cofer <samcofer@gmail.com>
2024-03-22 12:54:02 -0500

    Add milliseconds to python testing

--- scripts/pytorch.sh
+++ scripts/pytorch.sh
@@ -1,32 +1,32 @@
 echo BEGIN - Running in "$(pwd)"
 
 # =====================================
-# Set up Python virtual env for project. 
+# Set up Python virtual env for project.
 # =====================================
 # Delete venv folder if already exists
 VENV_DIR=.venv
 if [ -d $VENV_DIR ]; then
-    START=$(date +%s)
-    rm -Rf $VENV_DIR 
-    END=$(date +%s)
-    echo VENV REMOVE: Run duration = "$(date -u -d "0 $END seconds - $START seconds" +"%H:%M:%S")"
+    START=$(date +%s%N)
+    rm -Rf $VENV_DIR
+    END=$(date +%s%N)
+    echo VENV REMOVE: Run duration = "$(( ($END - $START) / 1000000 )) milliseconds"
 fi
 
 # Create venv
-START=$(date +%s)
+START=$(date +%s%N)
 python -m venv $VENV_DIR
-END=$(date +%s)
-echo VENV CREATE: Run duration = "$(date -u -d "0 $END seconds - $START seconds" +"%H:%M:%S")"
+END=$(date +%s%N)
+echo VENV CREATE: Run duration = "$(( ($END - $START) / 1000000 )) milliseconds"
 
 # =====================================
 # Activate and Update
 # which python; which pip;
 # =====================================
-START=$(date +%s)
+START=$(date +%s%N)
 source "${VENV_DIR}/bin/activate"
 python -m pip install --quiet --upgrade pip wheel setuptools
-END=$(date +%s)
-echo PIP UPDATE: Run duration = "$(date -u -d "0 $END seconds - $START seconds" +"%H:%M:%S")"
+END=$(date +%s%N)
+echo PIP UPDATE: Run duration = "$(( ($END - $START) / 1000000 )) milliseconds"
 
 # =====================================
 # Install Python torch package
@@ -34,18 +34,18 @@ echo PIP UPDATE: Run duration = "$(date -u -d "0 $END seconds - $START seconds" 
 #   Run to set temp folder if needed - `TMPDIR=/opt/rspro/tmp`
 #   Run to set temp folder if needed - `TMPDIR=/opt/tmpdir`
 # =====================================
-START=$(date +%s)
+START=$(date +%s%N)
 pip install --quiet torch
-END=$(date +%s)
-echo PYTORCH INSTALL: Run duration = "$(date -u -d "0 $END seconds - $START seconds" +"%H:%M:%S")"
+END=$(date +%s%N)
+echo PYTORCH INSTALL: Run duration = "$(( ($END - $START) / 1000000 )) milliseconds"
 
 # =====================================
 # Clean up
 # =====================================
 deactivate
-START=$(date +%s)
-rm -Rf $VENV_DIR   
-END=$(date +%s)
-echo CLEANUP: Run duration = "$(date -u -d "0 $END seconds - $START seconds" +"%H:%M:%S")"
+START=$(date +%s%N)
+rm -Rf $VENV_DIR
+END=$(date +%s%N)
+echo CLEANUP: Run duration = "$(( ($END - $START) / 1000000 )) milliseconds"
 
 echo END
